c6c8a136679c59b0061d238371a23f114b3afe78
cristian <ch374185@gmail.com>
2023-09-08 14:58:32 -0600

    03 Operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+// Operadores matematicos
+
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1/1);
+console.log(1%1);
+console.log(3**3);
+
+// Mas operadores
+let a = 10;
+console.log(a);
+a +=10; // a = a + 10
+console.log(a);
+a -=10;// a = a - 10
+console.log(a);
+a *=10;// a = a * 10
+console.log(a);
+a /=10;// a = a / 10
+console.log(a);
+a %=10;// a = a % 10
+console.log(a);
+
+// Operadores relacionales
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 !== 10);
+console.log(10 === 10);
+
+// Operadores Logicos
+console.log(true &&true);
+console.log(true || true);
+console.log(!true);
+
