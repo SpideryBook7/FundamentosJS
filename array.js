9c03be380dac6628a1d9d392fbba38c98ba983fe
cristian <ch374185@gmail.com>
2023-09-19 11:38:19 -0600

    08 arrays

---
+++ array.js
@@ -0,0 +1,36 @@
+// array en javascript
+const miArray = [1, 2, 4, true, 'hello', 'a', 2.1, [1, 1]]
+console.log(miArray);
+console.log(miArray[0]);
+console.log(miArray[1]);
+miArray[0] = "one";
+miArray[3, false];
+console.log(miArray);
+
+// pilas en js
+// last in first out 
+// ingresar valores al final 
+// extraer valores del final
+//pop, shift, unshift, push
+const pila = [] ;
+//insertar con
+pila.push(1);
+pila.push(2);
+pila.push(3);
+console.log(pila)
+console.log(pila.pop())
+console.log(pila)
+console.log(pila.pop())
+console.log(pila)
+
+// colas en js
+// first in first out
+const cola = []
+cola.unshift(1);
+cola.unshift(2);
+cola.unshift(3);
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
