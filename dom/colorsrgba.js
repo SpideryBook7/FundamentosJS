bd3cf4e2387c8e6676e8edd4c1fadec0d6649f30
cristian <ch374185@gmail.com>
2023-09-27 09:38:30 -0600

    15 DOM colores RGBA

---
+++ dom/colorsrgba.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const btnColor = document.querySelector('#btnColor');
+const red = document.querySelector('#red');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
+const trans = document.querySelector('#trans');
+
+function setColor(){
+    
+    // rgb(0, 255, 14);
+
+    const redVal = Math.floor(red.value);
+    const greenVal = Math.floor(green.value / 100 * 255);
+    const blueVal =Math.floor(blue.value / 100 * 255);
+    const transVal = trans.value / 100;
+    const rgbaColor = `rgb(${ redVal }, ${ greenVal }, ${ blueVal })`;
+    body.style.background = rgbaColor;
+}
+
+setColor();
+btnColor.addEventListener('click', setColor);
+
